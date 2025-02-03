adccbe01292dd42c3bb249b4bd96142121867887
ugotchriscoded <chrisdobadoglez@gmail.com>
2025-02-03 15:27:04 +0100

    feat: modify web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Container\Attributes\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
